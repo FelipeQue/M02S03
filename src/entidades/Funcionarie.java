5a216560e21575de768b15f505f7e25dd2f694d4
Felipe Quérette <felipe.querette@gmail.com>
2024-05-24 18:53:14 -0300

    Criada a classe Funcionarie

---
+++ src/entidades/Funcionarie.java
@@ -0,0 +1,52 @@
+package entidades;
+
+public class Funcionarie {
+
+    // Atributos
+    private String nome;
+    private int idade;
+    private Endereco endereco;
+    private double salario;
+
+    // Construtores
+    public Funcionarie() {
+    }
+
+    public Funcionarie(String nome, int idade, Endereco endereco) {
+        this.nome = nome;
+        this.idade = idade;
+        this.endereco = endereco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+}

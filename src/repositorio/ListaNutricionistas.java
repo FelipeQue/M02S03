326250079e62538d56daa6ccc6714c017c941b72
Felipe Quérette <felipe.querette@gmail.com>
2024-05-26 17:34:34 -0300

    Criada Lista de Nutricionistas

--- src/repositorio/ListaNutricionistas.java
+++ src/repositorio/ListaNutricionistas.java
@@ -1,2 +1,50 @@
-package repositorio;public class ListaNutricionistas {
+package repositorio;
+
+import entidades.Nutricionista;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaNutricionistas {
+
+    private static List<Nutricionista> nutricionistas;
+
+    static {
+        nutricionistas = new ArrayList<>();
+    }
+
+    public static List<Nutricionista> listarNutricionistas(){
+        return nutricionistas;
+    }
+
+    public static Nutricionista buscarNutricionistaId(int id){
+        return nutricionistas.get(id);
+    }
+
+    public static Nutricionista buscarNutricionistaNome(String nome){
+        return (Nutricionista) nutricionistas.stream()
+                .filter(n -> n.getNome().equals(nome))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public static void adicionarNutricionista(Nutricionista nutricionista) {
+        nutricionistas.add(nutricionista);
+    }
+
+    public static void removerNutricionista(int id){
+        nutricionistas.remove(id);
+    }
+
+    public static void alterarNutricionista(Nutricionista nutricionistaAlterade, int id) {
+        Nutricionista nutricionistaOriginal = nutricionistas.get(id);
+        nutricionistaOriginal.setConsultas(nutricionistaAlterade.getConsultas());
+        nutricionistaOriginal.setTempoExperiencia(nutricionistaAlterade.getTempoExperiencia());
+        nutricionistaOriginal.setCertificacoes(nutricionistaAlterade.getCertificacoes());
+        nutricionistaOriginal.setEndereco(nutricionistaAlterade.getEndereco());
+        nutricionistaOriginal.setNome(nutricionistaAlterade.getNome());
+        nutricionistaOriginal.setIdade(nutricionistaAlterade.getIdade());
+        nutricionistaOriginal.setSalario(nutricionistaAlterade.getSalario());
+    }
+
 }

0f461c81645b7b9ed706232eb878af1ec9d034d6
Felipe Quérette <felipe.querette@gmail.com>
2024-05-26 17:05:52 -0300

    Criada a ListaConsultas

--- src/repositorio/ListaConsultas.java
+++ src/repositorio/ListaConsultas.java
@@ -1,2 +1,47 @@
-package repositorio;public class ListaConsultas {
+package repositorio;
+
+import entidades.Consulta;
+import entidades.Nutricionista;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaConsultas {
+
+    private static List<Consulta> consultas;
+
+    static {
+        consultas = new ArrayList<>();
+    }
+
+    public static List<Consulta> listarConsultas(){
+        return consultas;
+    }
+
+    public static Consulta buscarConsultaId(int id){
+        return consultas.get(id);
+    }
+
+    public static void adicionarConsulta(Consulta consulta){
+        // Sempre que adicionarmos uma consulta, o nutricionista dessa consulta deve ter seu número de consultas incrementado.
+        Nutricionista nutricionista = ListaNutricionistas.buscarNutricionistaNome(consulta.getNutricionista());
+        if (nutricionista != null) {
+            int numeroConsultas = nutricionista.getConsultas() + 1;
+            nutricionista.setConsultas(numeroConsultas);
+        }
+        consultas.add(consulta);
+    }
+
+    public static void removerConsulta(int id){
+        consultas.remove(id);
+    }
+
+    public static void alterarConsulta(Consulta consultaAlterada, int id) {
+        Consulta consultaOriginal = consultas.get(id);
+        consultaOriginal.setNutricionista(consultaAlterada.getNutricionista());
+        consultaOriginal.setPaciente(consultaAlterada.getPaciente());
+        consultaOriginal.setDataHora(consultaAlterada.getDataHora());
+        consultaOriginal.setRealizada(consultaAlterada.isRealizada());
+    }
+
 }

45b7de99027754454a37ae9e6f72bb9b97d696c5
Felipe Quérette <felipe.querette@gmail.com>
2024-05-26 15:21:53 -0300

    Criada a classe Nutricionista

---
+++ src/entidades/Nutricionista.java
@@ -0,0 +1,50 @@
+package entidades;
+
+import java.util.List;
+
+public class Nutricionista extends Funcionarie{
+    // Atributos
+    private List<String> certificacoes;
+    private int consultas; // Número de consultas
+    private int tempoExperiencia; // Tempo de experiência em anos completos
+
+    //Construtores
+    public Nutricionista() {
+    }
+
+    public Nutricionista(String nome, int idade, Endereco endereco, List<String> certificacoes, int consultas, int tempoExperiencia) {
+        super(nome, idade, endereco);
+        this.certificacoes = certificacoes;
+        this.consultas = consultas;
+        this.tempoExperiencia = tempoExperiencia;
+    }
+
+
+    // Métodos
+
+
+    // Getters e setters
+    public List<String> getCertificacoes() {
+        return certificacoes;
+    }
+
+    public void setCertificacoes(List<String> certificacoes) {
+        this.certificacoes = certificacoes;
+    }
+
+    public int getConsultas() {
+        return consultas;
+    }
+
+    public void setConsultas(int consultas) {
+        this.consultas = consultas;
+    }
+
+    public int getTempoExperiencia() {
+        return tempoExperiencia;
+    }
+
+    public void setTempoExperiencia(int tempoExperiencia) {
+        this.tempoExperiencia = tempoExperiencia;
+    }
+}
